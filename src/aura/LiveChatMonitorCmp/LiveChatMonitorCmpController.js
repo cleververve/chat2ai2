b7641f476ea6d986b2629d590f87dbe6aaf28fc3
svolynets <svolynets@cleververve.com>
2021-10-29 17:50:02 +0300

    commit

---
+++ src/aura/LiveChatMonitorCmp/LiveChatMonitorCmpController.js
@@ -0,0 +1,73 @@
+/**
+ Author:         serhiivolynets
+ Date:           31.08.2021
+ Description:
+ **/
+({
+    // doInit : function(component, event, helper) {
+    //     var action = component.get("c.insertAudit");
+    //     action.setParams({"recordId":component.get("v.recordId")});
+    //     action.setCallback(this, function(response) {
+    //         var state = response.getState();
+    //         // Display toast message to indicate load status
+    //         var toastEvent = $A.get("e.force:showToast");
+    //         if (state === 'SUCCESS'){
+    //             toastEvent.setParams({
+    //                 "title": "Success!",
+    //                 "message": " Audit Log is created ."
+    //             });
+    //         }
+    //         else {
+    //             toastEvent.setParams({
+    //                 "title": "Error!",
+    //                 "message": " Something has gone wrong."
+    //             });
+    //         }
+    //         toastEvent.fire();
+    //     });
+    //     $A.enqueueAction(action);
+    // }
+
+    onAgentSend: function (cmp, evt, helper) {
+        var recordId = evt.getParam("recordId");
+        var content = evt.getParam("content");
+        var name = evt.getParam("name");
+        var type = evt.getParam("type");
+        var timestamp = evt.getParam("timestamp");
+        console.log("recordId:" + recordId + " content:" + content + " name:" + name + " timestamp:" + timestamp);
+        var action = cmp.get("c.logAgentReply");
+        action.setParams({"recordId": recordId, "content": content, "name": name, "type": type});
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            // Display toast message to indicate load status
+            // var toastEvent = $A.get("e.force:showToast");
+            if (state === 'SUCCESS') {
+                // toastEvent.setParams({
+                //     "title": "Success!",
+                //     "message": " Audit Log is created ."
+                // });
+            } else {
+                // toastEvent.setParams({
+                //     "title": "Error!",
+                //     "message": " Something has gone wrong."
+                // });
+            }
+            // toastEvent.fire();
+        });
+        $A.enqueueAction(action);
+    },
+    onChatEnded: function (cmp, evt, helper) {
+        // var recordId = evt.getParam("recordId");
+        //
+        // console.log("recordId:" + recordId);
+        // var action = cmp.get("c.chatEnded");
+        // action.setParams({"recordId": recordId});
+        // action.setCallback(this, function (response) {
+        //     var state = response.getState();
+        //     if (state === 'SUCCESS') {
+        //     } else {
+        //     }
+        // });
+        // $A.enqueueAction(action);
+    }
+})
---
+++ src/triggers/LiveChatTranscript.trigger
@@ -0,0 +1,21 @@
+/**
+Author:         serhiivolynets
+Date:           24.08.2021
+Description:   
+**/
+
+trigger LiveChatTranscript on LiveChatTranscript (before insert, before update)
+{
+//	if(Trigger.isUpdate)
+//	{
+//		system.debug('LiveChatTranscript.update');
+//	}
+//	else if(Trigger.isInsert)
+//	{
+//		system.debug('LiveChatTranscript.insert');
+//	}
+//	for(LiveChatTranscript tr: trigger.new)
+//	{
+//		tr.
+//	}
+}
